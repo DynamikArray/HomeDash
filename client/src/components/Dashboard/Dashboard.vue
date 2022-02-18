ceb11a486f9333a983f603ad4ac6fee5a2df7fdd
DynamikArray <dynamikarray@gmail.com>
2022-02-17 20:44:13 -0500

    Testing vue-grid

--- client/src/components/Dashboard/Dashboard.vue
+++ client/src/components/Dashboard/Dashboard.vue
@@ -1,11 +1,93 @@
 <template>
-  <div>Dashboard</div>
+  <section>
+    <div>Dashboard</div>
+    <div class="hd-gridLayout">
+      <grid-layout
+        :layout.sync="layout"
+        :col-num="12"
+        :row-height="30"
+        :is-draggable="true"
+        :is-resizable="true"
+        :is-mirrored="false"
+        :vertical-compact="true"
+        :margin="[10, 10]"
+        :use-css-transforms="true"
+      >
+        <grid-item v-for="item in layout" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i" :key="item.i">
+          {{ item.i }}
+          <h4>CONTENT</h4>
+        </grid-item>
+      </grid-layout>
+    </div>
+  </section>
 </template>
 
 <script>
+import VueGridLayout from "vue-grid-layout";
 export default {
   name: "hd-Dashboard",
+  components: {
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem,
+  },
+  data: () => ({
+    layout: [
+      { x: 0, y: 0, w: 2, h: 4, i: "0" },
+      { x: 2, y: 0, w: 2, h: 4, i: "1" },
+      { x: 4, y: 0, w: 2, h: 5, i: "2" },
+      { x: 6, y: 0, w: 2, h: 3, i: "3" },
+      { x: 8, y: 0, w: 2, h: 3, i: "4" },
+      { x: 10, y: 0, w: 2, h: 3, i: "5" },
+      { x: 0, y: 5, w: 2, h: 5, i: "6" },
+      { x: 2, y: 5, w: 2, h: 5, i: "7" },
+      { x: 4, y: 5, w: 2, h: 5, i: "8" },
+      { x: 6, y: 3, w: 2, h: 4, i: "9" },
+      { x: 8, y: 4, w: 2, h: 4, i: "10" },
+      { x: 10, y: 4, w: 2, h: 4, i: "11" },
+      { x: 0, y: 10, w: 2, h: 5, i: "12" },
+      { x: 2, y: 10, w: 2, h: 5, i: "13" },
+      { x: 4, y: 8, w: 2, h: 4, i: "14" },
+      { x: 6, y: 8, w: 2, h: 4, i: "15" },
+      { x: 8, y: 10, w: 2, h: 5, i: "16" },
+      { x: 10, y: 4, w: 2, h: 2, i: "17" },
+      { x: 0, y: 9, w: 2, h: 3, i: "18" },
+      { x: 2, y: 6, w: 2, h: 2, i: "19" },
+    ],
+  }),
 };
 </script>
 
-<style scoped></style>
+<style>
+.vue-grid-item {
+  background: #777;
+}
+
+.vue-grid-item.vue-grid-placeholder {
+  background: #eee;
+  opacity: 0.2;
+  transition-duration: 100ms;
+  z-index: 2;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+  border: 1px solid #eee;
+}
+
+.vue-draggable-handle {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  top: 0;
+  left: 0;
+  background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#FFFFFF'/></svg>")
+    no-repeat;
+  background-position: bottom right;
+  padding: 0 8px 8px 0;
+  background-repeat: no-repeat;
+  background-origin: content-box;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+</style>
